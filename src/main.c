c37c7ef8789f9e65bbb8169e2e8b23f21010b45e
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2024-09-08 16:59:51 -0700

    Connect to display

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <wayland-client.h>
 
-int main() {
-    printf("hi\n");
+int main(int argc, char *argv[]) {
+    struct wl_display *display = wl_display_connect(NULL);
+    if (!display) {
+        printf("Failed to connect to Wayland display.\n");
+        return 1;
+    }
+    printf("Connection established!\n");
+
+    wl_display_disconnect(display);
     return 0;
 }
 
